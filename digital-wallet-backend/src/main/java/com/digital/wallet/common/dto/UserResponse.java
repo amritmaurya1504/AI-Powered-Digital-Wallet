7b7c7df7d3a82e2173b8a13c46fe945ac77b038a
iRE-X <harekrishna8016@gmail.com>
2026-08-12 00:09:18 +0530

    refactor: made changes as required for JWT creation

--- digital-wallet-backend/src/main/java/com/digital/wallet/common/dto/UserResponse.java
+++ digital-wallet-backend/src/main/java/com/digital/wallet/common/dto/UserResponse.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 public record UserResponse(
         Long id,
+        String name,
         String email,
         String phone,
         Set<RoleType> roles
--- digital-wallet-backend/src/main/java/com/digital/wallet/user/service/UserService.java
+++ digital-wallet-backend/src/main/java/com/digital/wallet/user/service/UserService.java
@@ -2,9 +2,11 @@ package com.digital.wallet.user.service;
 
 import com.digital.wallet.common.dto.CreateUserRequest;
 import com.digital.wallet.common.dto.UserResponse;
+import com.digital.wallet.user.domain.User;
 import com.digital.wallet.user.dto.UpdateUserRequest;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserService {
     UserResponse createUser(CreateUserRequest createUserRequest);
@@ -12,4 +14,6 @@ public interface UserService {
     UserResponse getUserById(Long userId);
     UserResponse updateUser(Long userId, UpdateUserRequest updateUserRequest);
     void deleteUser(Long userId);
+
+    Optional<User> getUserByEmail(String email);
 }
